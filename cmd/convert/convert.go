8a962ecb752bdb1bf22c3922207d14096d3aa2b7
AbdulazizAli3 <abdulaziz.ali@softwarfare.com>
2024-05-16 17:54:17 -0500

    convert command can now cover video to mp3

--- cmd/convert/convert.go
+++ cmd/convert/convert.go
@@ -5,6 +5,8 @@ package convert
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -15,12 +17,21 @@ var ConvertCmd = &cobra.Command{
 	Short: "Converts video files to mp3",
 	Long: `Converts video files to mp3 
 For example:
-Vibeo-cli convert -f example/directory/video`,
+Vibeo-cli convert -f example/directory/video.mp4 -o audio_file`,
 	Run: func(cmd *cobra.Command, args []string) {
 		File, _ := cmd.Flags().GetString("file")
+		Output, _ := cmd.Flags().GetString("out")
 
 		if File != "None" {
-			fmt.Println(File)
+			fmt.Println("Converting file...")
+			Output += ".mp3"
+			exec_cmd := exec.Command("ffmpeg", "-i", File, "-vn", "-acodec", "libmp3lame", "-ab", "192k", "-ar", "44100", "-y", Output)
+			b, err := exec_cmd.CombinedOutput()
+			if err != nil {
+				log.Printf("Convertion failed %v", err)
+			}
+
+			fmt.Printf("%s\n", b)
 		} else {
 			cmd.Help()
 		}
@@ -30,4 +41,5 @@ Vibeo-cli convert -f example/directory/video`,
 
 func init() {
 	ConvertCmd.Flags().StringP("file", "f", "None", "Path to file desired for conversion")
+	ConvertCmd.Flags().StringP("out", "o", "audio", "Name of the converted file")
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module Vibeo-cli
 go 1.22.3
 
 require (
+	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
