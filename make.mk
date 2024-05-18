9f2dea548fa88871f045932769e8c49dfa841c0e
AbdulazizAli3 <abdulaziz.ali@softwarfare.com>
2024-05-18 10:42:21 -0500

    add a check for if python is installed

--- make.mk
+++ make.mk
@@ -1,7 +1,13 @@
 
+#check if python is installed
+check_python:
+	@python3 -c "import sys; \
+	exit_code = 0 if sys.version_info >= (3, 0) else 1; \
+	sys.exit(exit_code)" || (echo "Python 3 is required but not found. Please install Python 3."; exit 1)
+
 
 # Default target to build the application
-build:
+build: check_python
 	go build -o Vibeo-Cli main.go
 
 # Target to clean up built files
